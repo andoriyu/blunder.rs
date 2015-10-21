cd9e52dc180c36437cad47fa9c1e0345090c0f6b
Andrey Cherkashin <with.out@me.com>
2015-10-21 11:57:18 -0800

    Update documentation

--- implementors/num/traits/trait.FromPrimitive.js
+++ implementors/num/traits/trait.FromPrimitive.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['num'] = [];implementors['blunder'] = ["impl <a class='trait' href='num/traits/trait.FromPrimitive.html' title='num::traits::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='blunder/enum.BsdError.html' title='blunder::BsdError'>BsdError</a>",];
+implementors['num'] = [];implementors['blunder'] = ["impl <a class='trait' href='num/traits/trait.FromPrimitive.html' title='num::traits::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='blunder/enum.BsdError.html' title='blunder::BsdError'>BsdError</a>",];implementors['blunder'] = ["impl <a class='trait' href='num/traits/trait.FromPrimitive.html' title='num::traits::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='blunder/enum.BsdError.html' title='blunder::BsdError'>BsdError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
