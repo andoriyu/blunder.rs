1befac9d91d94fb4ae5a8a24fe2faf15b72c02c7
Andrey Cherkashin <with.out@me.com>
2016-01-14 14:09:32 -0800

    Fixed macro yo

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,17 @@ use std::error::Error as StdError;
 use std::fmt;
 use std::result::Result;
 use std::ops::Deref;
+use std::convert::From;
 
 
 mod bsd;
 
 pub use bsd::*;
 
-/// Macro helper to propagate an error
 #[macro_export]
 macro_rules! fail {
     ($expr:expr) => (
-        return Err(::std::error::FromError::from_error($expr));
+        return Result::Err(::std::convert::From::from($expr));
         )
 }
 
@@ -35,22 +35,22 @@ macro_rules! maybe_fail {
 /// Generic af struct for errror handling
 /// Designed to host anything that implements error::Error trait
 /// Yet can host whatever (like errno from libc)
-#[derive(Debug)]
-pub struct Blunder<T> {
+#[derive(Debug, PartialEq)]
+pub struct Blunder<T: StdError> {
     /// How to identify the error
     kind: T,
     detail: Option<String>
 }
 
 /// Because we want easy switch/case on kind...
-impl <T> Deref for Blunder<T> {
+impl <T: StdError> Deref for Blunder<T> {
     type Target = T;
 
     fn deref<'a>(&'a self) -> &'a T {
         &self.kind
     }
 }
-impl <T> Blunder<T> {
+impl <T: StdError> Blunder<T> {
     /// Optional reasoning behind such behavior.
     /// Think "Client doesn't understand XXX cipher"
     pub fn detail(&self) -> Option<String> {
@@ -69,6 +69,11 @@ impl <T: StdError> StdError for Blunder<T> {
 impl <T: StdError> fmt::Display for Blunder<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f,"{}", self.description())
+    }
+}
+impl <E: StdError> From<E> for Blunder<E> {
+    fn from(err: E) -> Blunder<E> {
+        Blunder { kind: err, detail: None }
     }
 }
 
@@ -96,4 +101,17 @@ fn it_works() {
     assert_eq!(error.cause().is_some(), false);
     assert_eq!(error.description(), "wat");
     assert_eq!(*error, Wat::One);
+
+    // Test fail! macro
+
+    fn goto_fail() -> Result<(), Blunder<Wat>> {
+        fail!(Wat::One)
+    };
+
+    let fail = Blunder { kind: Wat::One, detail: None };
+    if let Err(err) = goto_fail() {
+        assert_eq!(err, fail);
+    } else {
+        panic!();
+    }
 }
